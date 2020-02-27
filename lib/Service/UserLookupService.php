b837a3c5b265391d375a5d9c3c2de4bf997b53a0
moddavid <mod89@gmx.net>
2020-02-27 18:33:39 +0100

    Added 'add-missing-user' option to create new user on login

--- lib/Service/UserLookupService.php
+++ lib/Service/UserLookupService.php
@@ -26,55 +26,70 @@ use OC\User\LoginException;
 use OCA\OpenIdConnect\Client;
 use OCP\IUser;
 use OCP\IUserManager;
+use phpseclib\Crypt\Random;
 
 class UserLookupService {
-
-	/**
-	 * @var IUserManager
-	 */
-	private $userManager;
-	/**
-	 * @var Client
-	 */
-	private $client;
-
-	public function __construct(IUserManager $userManager,
-								Client $client) {
-		$this->userManager = $userManager;
-		$this->client = $client;
-	}
-
-	/**
-	 * @param mixed $userInfo
-	 * @return IUser
-	 * @throws LoginException
-	 * @throws HintException
-	 */
-	public function lookupUser($userInfo): IUser {
-		$openIdConfig = $this->client->getOpenIdConfig();
-		if ($openIdConfig === null) {
-			throw new HintException('Configuration issue in openidconnect app');
-		}
-		$searchByEmail = true;
-		if (isset($openIdConfig['mode']) && $openIdConfig['mode'] === 'userid') {
-			$searchByEmail = false;
-		}
-		$attribute = $openIdConfig['search-attribute'] ?? 'email';
-
-		if ($searchByEmail) {
-			$user = $this->userManager->getByEmail($userInfo->$attribute);
-			if (!$user) {
-				throw new LoginException("User with {$userInfo->$attribute} is not known.");
-			}
-			if (\count($user) !== 1) {
-				throw new LoginException("{$userInfo->$attribute} is not unique.");
-			}
-			return $user[0];
-		}
-		$user = $this->userManager->get($userInfo->$attribute);
-		if (!$user) {
-			throw new LoginException("User {$userInfo->$attribute} is not known.");
-		}
-		return $user;
-	}
+    
+    /**
+     * @var IUserManager
+     */
+    private $userManager;
+    /**
+     * @var Client
+     */
+    private $client;
+    
+    public function __construct(IUserManager $userManager,
+        Client $client) {
+            $this->userManager = $userManager;
+            $this->client = $client;
+    }
+    
+    /**
+     * @param mixed $userInfo
+     * @return IUser
+     * @throws LoginException
+     * @throws HintException
+     */
+    public function lookupUser($userInfo): IUser {
+        $openIdConfig = $this->client->getOpenIdConfig();
+        if ($openIdConfig === null) {
+            throw new HintException('Configuration issue in openidconnect app');
+        }
+        $searchByEmail = true;
+        if (isset($openIdConfig['mode']) && $openIdConfig['mode'] === 'userid') {
+            $searchByEmail = false;
+        }
+        $attribute = $openIdConfig['search-attribute'] ?? 'email';
+        
+        if ($searchByEmail) {
+            $user = $this->userManager->getByEmail($userInfo->$attribute);
+            if (!$user) {
+                $addMissingUser = $openIdConfig['add-missing-user'] ?? FALSE;
+                if (!$addMissingUser) {
+                    throw new LoginException("User with {$userInfo->$attribute} is not known.");
+                } else {
+                    $user = $this->userManager->createUser($userInfo->preferred_username, bin2hex(\phpseclib\Crypt\Random::string(8)));
+                    $user->setEMailAddress($userInfo->email);
+                    $user->setDisplayName($userInfo->name);
+                    $user->setEnabled(TRUE);
+                    
+                    $user->canChangeDisplayName(FALSE);
+                    $user->canChangePassword(FALSE);
+                    
+                    $attribute = 'preferred_username';
+                    return $user;
+                }
+            }
+            if (\count($user) !== 1) {
+                throw new LoginException("{$userInfo->$attribute} is not unique.");
+            }
+            return $user[0];
+        }
+        $user = $this->userManager->get($userInfo->$attribute);
+        if (!$user) {
+            throw new LoginException("User {$userInfo->$attribute} is not known.");
+        }
+        return $user;
+    }
 }
